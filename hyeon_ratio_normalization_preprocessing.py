a9592c63c152f055db2ea5a956c0df2c69c1266b
Hyeoun <ing02084@naver.com>
2022-02-18 12:08:33 +0900

    테스트 전처리 파일 생성

--- hyeon_ratio_normalization_preprocessing.py
+++ hyeon_ratio_normalization_preprocessing.py
@@ -5,13 +5,15 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
 import pickle
 
-df = pd.read_csv('./resources/rock_scissor_paper_nomallize_data_w_640_h_480.csv')
+df = pd.read_csv('./resources/파일명.csv')  # 추후 파일명에 맞게 수정
 
 print(df.head())
 df.info()
 
 Y = df['category']  # 카테고리 y값으로 추출
 X = df.loc[:, df.columns != 'category']  # 카테고리를 제외한 X값 추출
+
+X = X / 2.5  # 비율 정규화
 
 X.info()
 X = X.to_numpy()
@@ -23,18 +25,18 @@ encoder = LabelEncoder()
 labeled_y = encoder.fit_transform(Y)  # 라벨을 숫자에 대응
 label = encoder.classes_  # 라벨 - 숫자 목록
 
-with open('./resources/encoder_ro_sci_pa.pickle', 'wb') as f:
+with open('./resources/test_encoder_ratio_normalization_lbl.pickle', 'wb') as f:
     pickle.dump(encoder, f)
 
 onehot_Y = to_categorical(labeled_y)  # onehotencoding
 print(X.shape)
 print(onehot_Y.shape)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, onehot_Y, test_size=0.05)  # 테스트 데이터 분리
+X_train, X_test, Y_train, Y_test = train_test_split(X, onehot_Y, test_size=0.1)  # 테스트 데이터 분리
 print(X_train.shape, Y_train.shape)
 print(X_test.shape, Y_test.shape)
 
 # exit()
 xy = X_train, X_test, Y_train, Y_test
 
-np.save('./resources/test_ro_sci_pa_preprocessing_data', xy)
+np.save('./resources/test_encoder_ratio_normalization_data', xy)
