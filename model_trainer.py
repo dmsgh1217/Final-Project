ebc4dae1d07dda59ed1d077a3c17a34865722a0b
PoiPo2 <kimmc0316@gmail.com>
2022-02-24 17:40:15 +0900

    Dense Layer 기반 인공지능 학습 모델입니다.

--- model_trainer.py
+++ model_trainer.py
@@ -1,3 +1,8 @@
+# 전처리된 데이터를 활용하여 인공지능 모델을 학습하는 스크립트 입니다.
+
+# Release 1.0 by Min-chul
+
+
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
@@ -16,9 +21,13 @@ def train_model(**kwargs):
     filename = kwargs['filename'] if 'filename' in kwargs else 'model'
     # 현재 작업중인 디렉토리에 "models" 디렉토리가 없는 경우, 디렉토리를 생성합니다.
     os.makedirs('models', exist_ok=True)
-    print(f'input_shape: {x_train[0].shape}')
+
+    # 모델의 입력 차원을 정의합니다.
+    input_shape = x_train[0].shape
+    print(f'input_shape: {input_shape}')
     if model_sequence == 1:
-        network = Sequential([Dense(256, input_shape=x_train[0].shape, activation='relu'),
+        epoch = 150
+        network = Sequential([Dense(256, input_shape=input_shape, activation='relu'),
                               Dropout(0.3),
                               Dense(128, activation='relu'),
                               Dropout(0.2),
@@ -30,14 +39,16 @@ def train_model(**kwargs):
                               Dense(2, activation='softmax')])
         network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         network.summary()
-        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
-                                      callbacks=[ModelCheckpoint(f'./models/{filename}.h5', monitor='val_accuracy',
-                                                                 verbose=1, save_best_only=True, mode='auto'),
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto'),
                                                  ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
                                                                    verbose=1, mode='auto')])
         return network, network_history
     elif model_sequence == 2:
-        network = Sequential([Dense(256, input_shape=x_train[0].shape, activation='relu'),
+        epoch = 300
+        network = Sequential([Dense(256, input_shape=input_shape, activation='relu'),
                               Dropout(0.3),
                               Dense(128, activation='relu'),
                               Dropout(0.2),
@@ -49,16 +60,112 @@ def train_model(**kwargs):
                               Dense(2, activation='softmax')])
         network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         network.summary()
-        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
-                                      callbacks=[ModelCheckpoint(f'./models/{filename}.h5', monitor='val_accuracy',
-                                                                 verbose=1, save_best_only=True, mode='auto'),
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto'),
+                                                 ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
+                                                                   verbose=1, mode='auto')])
+        return network, network_history
+    elif model_sequence == 3:
+        epoch = 300
+        network = Sequential([Dense(512, input_shape=input_shape, activation='relu'),
+                              Dropout(0.25),
+                              Dense(256, activation='relu'),
+                              Dropout(0.2),
+                              Dense(128, activation='relu'),
+                              Dropout(0.2),
+                              Dense(64, activation='relu'),
+                              Dropout(0.2),
+                              Dense(32, activation='relu'),
+                              Dropout(0.1),
+                              Dense(16, activation='relu'),
+                              Dense(2, activation='softmax')])
+        network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        network.summary()
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto'),
+                                                 ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
+                                                                   verbose=1, mode='auto')])
+        return network, network_history
+    # 3번 모델에서 학습률(Learning Rate) 통제 정책을 사용하지 않도록 변경
+    # 3번 모델 대비 성능 악화(0.8764)
+    elif model_sequence == 4:
+        epoch = 200
+        network = Sequential([Dense(512, input_shape=input_shape, activation='relu'),
+                              Dropout(0.25),
+                              Dense(256, activation='relu'),
+                              Dropout(0.2),
+                              Dense(128, activation='relu'),
+                              Dropout(0.2),
+                              Dense(64, activation='relu'),
+                              Dropout(0.2),
+                              Dense(32, activation='relu'),
+                              Dropout(0.1),
+                              Dense(16, activation='relu'),
+                              Dense(2, activation='softmax')])
+        network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        network.summary()
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto')])
+        return network, network_history
+    # 3번 모델에서 활성화 함수를 "ReLU"에서 "ELU"로 변경 (0.9976)
+    elif model_sequence == 5:
+        epoch = 200
+        network = Sequential([Dense(512, input_shape=input_shape, activation='elu'),
+                              Dropout(0.25),
+                              Dense(256, activation='elu'),
+                              Dropout(0.2),
+                              Dense(128, activation='elu'),
+                              Dropout(0.2),
+                              Dense(64, activation='elu'),
+                              Dropout(0.2),
+                              Dense(32, activation='elu'),
+                              Dropout(0.1),
+                              Dense(16, activation='elu'),
+                              Dense(2, activation='softmax')])
+        network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        network.summary()
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}_test1.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto'),
+                                                 ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
+                                                                   verbose=1, mode='auto')])
+        return network, network_history
+    # 5번 모델에서 활성화 함수를 "ELU"에서 "Swish"로 변경 (0.9989)
+    elif model_sequence == 6:
+        epoch = 200
+        network = Sequential([Dense(512, input_shape=input_shape, activation='swish'),
+                              Dropout(0.25),
+                              Dense(256, activation='swish'),
+                              Dropout(0.2),
+                              Dense(128, activation='swish'),
+                              Dropout(0.2),
+                              Dense(64, activation='swish'),
+                              Dropout(0.2),
+                              Dense(32, activation='swish'),
+                              Dropout(0.1),
+                              Dense(16, activation='swish'),
+                              Dense(2, activation='softmax')])
+        network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        network.summary()
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_seq{model_sequence}_test1.h5',
+                                                                 monitor='val_accuracy', verbose=1, save_best_only=True,
+                                                                 mode='auto'),
                                                  ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
                                                                    verbose=1, mode='auto')])
         return network, network_history
     else:
+        epoch = 100
         print(f'There is no model corresponding to "model_sequence({model_sequence})".'
               f'Training proceeds with the basic model.')
-        network = Sequential([Dense(256, input_shape=x_train[0].shape, activation='relu'),
+        network = Sequential([Dense(256, input_shape=input_shape, activation='relu'),
                               Dropout(0.3),
                               Dense(128, activation='relu'),
                               Dropout(0.2),
@@ -70,8 +177,8 @@ def train_model(**kwargs):
                               Dense(2, activation='softmax')])
         network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         network.summary()
-        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
-                                      callbacks=[ModelCheckpoint(f'./models/{filename}.h5', monitor='val_accuracy',
+        network_history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epoch,
+                                      callbacks=[ModelCheckpoint(f'./models/{filename}_based.h5', monitor='val_accuracy',
                                                                  verbose=1, save_best_only=True, mode='auto'),
                                                  ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10,
                                                                    verbose=1, mode='auto')])
@@ -81,11 +188,9 @@ def train_model(**kwargs):
 if __name__ == '__main__':
     # 학습 하려는 액션(category)을 정의합니다.
     action = ['move', 'click']
-    # 모델 학습량(epochs)을 정의합니다.
-    epochs = 150
 
     # 전처리된 데이터(.npy)를 불러옵니다.
-    x_train, x_test, y_train, y_test = np.load(file='./dataset/encoder_loc_data.npy', allow_pickle=True)
+    x_train, x_test, y_train, y_test = np.load(file='./resources/encoder_loc_data.npy', allow_pickle=True)
     print(f'x_train.shape: {x_train.shape}')
     print(f'y_train.shape: {y_train.shape}')
     print(f'x_test.shape: {x_test.shape}')
@@ -95,7 +200,7 @@ if __name__ == '__main__':
     # model_sequence: 구성한 인공지능 네트워크에 번호를 부여하여 전달한 번호의 네트워크에 학습을 진행합니다.
     # filename: 저장되는 모델의 이름을 지정합니다.
     model_name = 'test'
-    model, history = train_model(model_sequence=1, filename=model_name)
+    model, history = train_model(model_sequence=5, filename=model_name)
 
     # 학습 결과를 그래프로 시각화하여 표현합니다.
     plt.figure(figsize=(10, 12))
