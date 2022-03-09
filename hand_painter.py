21d9c4af444ab532bf0f605d0423c06d90a75f99
Hyeoun <ing02084@naver.com>
2022-03-09 11:42:54 +0900

    손동장 지우개 사용가능

--- hand_painter.py
+++ hand_painter.py
@@ -1,11 +1,12 @@
 import sys
 import numpy as np
-import cv2, test_ui_1, pyautogui
+import cv2, test_ui_1
 
 oldx = oldy = -1
 clocX, clocY = 0, 0
 iimg = np.ones((750, 1200, 3), dtype=np.uint8) * 255
 virtual_event = 'default'
+pen_toggle = True
 
 def on_mouse(event, x, y, flags, param):
     global oldx, oldy, virtual_event
@@ -27,24 +28,23 @@ def on_mouse(event, x, y, flags, param):
 
 
 def on_hand(event, x, y, flags, param):
-    global oldx, oldy, clocX, clocY
+    global oldx, oldy
 
-    clocX, clocY = pyautogui.position()
-    clocX, clocY = clocX-423, clocY-252
+    clocX, clocY = x, y
     if virtual_event == 'leftclick':
         oldx, oldy = clocX, clocY
     elif virtual_event == 'drag':
-        print(clocX, clocY)
-        print(oldx, oldy)
-        cv2.line(iimg, (oldx, oldy), (int(clocX), int(clocY)), (0, 0, 0), 3, cv2.LINE_AA)
+        if pen_toggle: cv2.line(iimg, (oldx, oldy), (int(clocX), int(clocY)), (0, 0, 0), 3, cv2.LINE_AA)
+        else: cv2.line(iimg, (oldx, oldy), (x, y), (255, 255, 255), 30, cv2.LINE_AA)
         oldx, oldy = clocX, clocY
 
 
 
 def main(param):
-    global virtual_event
+    global virtual_event, pen_toggle
     virtual_event = param
     cv2.namedWindow('painter')
+    if virtual_event == 'rightclick': pen_toggle = not pen_toggle
     if virtual_event in ['drag', 'leftclick']:
         cv2.setMouseCallback('painter', on_hand, iimg)
     else:
